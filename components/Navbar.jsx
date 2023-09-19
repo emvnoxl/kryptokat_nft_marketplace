5e153febebd6f002d03c2109e50fa49d58fba2a2
Emmanuel Thompson <emmanuelfiifithompson@gmail.com>
2023-09-19 08:57:40 +0000

    change assets folder and imports

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Images from '../assets';
+
+const Navbar = () => (
+  
+
+
+  return (
+  <div>Navbar</div>
+  )
+);
+
+export default Navbar;
